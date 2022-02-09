6e2a98288b4a44f7c5bba852087ca17da1a27f69
jxgonza <jorgexavier09@gmail.com>
2022-02-09 11:36:58 -0500

    Implemented bot that can tweet.

---
+++ twitter_bot.py
@@ -0,0 +1,22 @@
+from urllib import response
+import tweepy
+
+from twitter_keys import ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, twitter_secrets as ts
+from days_calculator import calculator
+
+# Assigns the keys from another file. 
+BEARER = ts.BEARER_TOKEN
+CONSUMER_KEY = ts.CONSUMER_KEY
+CONSUMER_SECRET = ts.CONSUMER_SECRET
+ACCESS_KEY = ts.ACCESS_KEY
+ACCESS_SECRET = ts.ACCESS_SECRET
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+client = tweepy.Client(bearer_token=BEARER, consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, access_token=ACCESS_KEY, access_token_secret=ACCESS_SECRET)
+
+days_until = calculator()
+day_text = "Quedan %s días para el concierto de #BadBunny en Orlando, FL. Te amo BIBR <3" %str(days_until)
+# client.create_tweet(text="Quedan %s días para el concierto de #BadBunny en Orlando, FL. Te amo BIBR <3" %str(days_until))
+client.create_tweet(text="hi my names josh")
